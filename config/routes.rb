0e7a77267731656519e46c8f7f08a2439ba13e3f
Ryan Leichliter <leichliter.ryan@gmail.com>
2014-09-15 15:32:52 -0500

    fixed some issues.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 TaskManagerProject::Application.routes.draw do
-
+  get "logout" => 'logins#destroy', :as => "logout"
+  
+  get "login" => 'logins#new', :as => "login"
+  post "login" => 'logins#create', :as => "create_login"
 
   resources :users
   resources :tasks
