fb10ae06353b3c8aa9dfea438d552198989d4049
“Kyle <kschmit90@gmail.com>
2014-09-15 15:39:23 -0500

    fixed merge error

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 TaskManagerProject::Application.routes.draw do
-<<<<<<< HEAD
   get "logout" => 'logins#destroy', :as => "logout"
   
   get "login" => 'logins#new', :as => "login"
   post "login" => 'logins#create', :as => "create_login"
 
-=======
->>>>>>> 0025b7816b1c2af044f7a3ee8107a16b0efceea5
   resources :users
   resources :tasks
 end
