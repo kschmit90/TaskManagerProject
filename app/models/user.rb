cb1393349874938d5a10b26a16b4a31025313392
Ryan Leichliter <leichliter.ryan@gmail.com>
2014-09-15 16:47:08 -0500

    Added validation for email and password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
   
+  validates :email, uniqueness: ture
+  validates :email, :password, :password_confirmation, pressence: true
+  
+  
   has_secure_password
 end
