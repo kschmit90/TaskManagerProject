fa7badef97b6ee3861fab6070d3c9c16389089ec
“Jen <mpjeno@gmail.com>
2014-09-15 17:02:43 -0500

    Added documentation to Task Model methods

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,6 +5,9 @@ class Task < ActiveRecord::Base
   
   validate :deadline_is_date?
   
+  # Changes the boolean saved in the "complete" column to a more user-friendly string
+  #
+  # Returns a user-friendly string describing whether the task is stored as complete or not
   def completed
     if complete == true
       "complete"
@@ -13,6 +16,9 @@ class Task < ActiveRecord::Base
     end
   end
 
+  # Validates that the user's input for deadline is a valid date
+  #
+  # Returns an error if it is not a valid date, otherwise it allows the new task to be created
   def deadline_is_date?
     if !deadline.is_a?(Date)
       errors.add(:deadline, 'must be a valid date') 
