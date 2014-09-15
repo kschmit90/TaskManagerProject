ad19897c566722f9ec56510dc264030ded7a1750
“Jen <mpjeno@gmail.com>
2014-09-15 14:37:54 -0500

    Removed SASS and Coffeescript

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,30 @@
 class TasksController < ApplicationController
   
   def index
+    @tasks = Task.all
   end
   
   def show
+    @task = Task.new
   end
   
   def new
+    @task = Task.new
+    
   end
   
   def create
+    @task = Task.new(params[:task])
+    
+    if @task.save
+      redirect_to task_path(@task.id), :notice => "New task saved! Way to go."
+    else
+      render "new"
+    end
   end
   
   def edit
+    
   end
   
   def update
