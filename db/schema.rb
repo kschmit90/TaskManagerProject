0f4e37556c7e8d43ae7b7435432510a362f60737
“Kyle <kschmit90@gmail.com>
2014-09-16 13:10:17 -0500

    created join table migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140915192758) do
+ActiveRecord::Schema.define(:version => 20140916175744) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "tasks", :force => true do |t|
     t.string   "name"
