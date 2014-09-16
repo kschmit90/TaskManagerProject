8e907d18c7e673519b9d384756758040b11f6789
Ryan Leichliter <leichliter.ryan@gmail.com>
2014-09-16 13:22:44 -0500

    Added name to User migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140915192758) do
+ActiveRecord::Schema.define(:version => 20140916181707) do
 
   create_table "tasks", :force => true do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20140915192758) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "name"
   end
 
 end
