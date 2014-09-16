e65344d263665e507fa17a3ad390e90fc9aead33
“Kyle <kschmit90@gmail.com>
2014-09-16 13:00:50 -0500

    set up HaBtM relationship, added resource to routes

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ActiveRecord::Base
   attr_accessible :description, :name
+  has_and_belongs_to_many :tasks
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ActiveRecord::Base
   attr_accessible :complete, :deadline, :name
+  has_and_belongs_to_many :categories
   
   scope :is_complete, where(:complete => true)
   
