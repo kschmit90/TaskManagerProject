2fbfa062e010e76385416643d6be8585bd9d3a78
“Jen <mpjeno@gmail.com>
2014-09-16 12:00:24 -0500

    Added validation for the presence of a project name

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,24 @@
 class ProjectsController < ApplicationController
+  
+  def index
+  end
+  
+  def show
+  end
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def create
+  end
+  
+  def destroy
+  end
+  
 end
