645fc80415315a79dfe261aa7666a9d8c4ce7358
“Kyle <kschmit90@gmail.com>
2014-09-16 13:15:18 -0500

    migrated categories_tasks join table

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140916175744) do
+ActiveRecord::Schema.define(:version => 20140916181000) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
     t.string   "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "categories_tasks", :force => true do |t|
+    t.integer "category_id"
+    t.integer "task_id"
   end
 
   create_table "tasks", :force => true do |t|
