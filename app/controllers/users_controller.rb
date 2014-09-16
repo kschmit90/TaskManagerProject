afbd87984208887fa72c8b08cd7f1ef9a34eb82c
Ryan Leichliter <leichliter.ryan@gmail.com>
2014-09-16 12:58:22 -0500

    Added edit and update functionality for Users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,4 +22,18 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    
+    if @user.update_attributes(params[:user])
+      redirect_to user_path(@user.id), :notice => "User info updated! Way to go."
+    else
+      render "edit"
+    end
+  end
 end
