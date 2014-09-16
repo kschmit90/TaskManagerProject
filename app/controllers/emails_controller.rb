bad8821cd27d5d18ab0c1190ccfaa090b63a434f
Ryan Leichliter <leichliter.ryan@gmail.com>
2014-09-16 16:51:47 -0500

    took email code out of email controller.

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,18 +1,5 @@
 class EmailsController < ApplicationController
   skip_before_filter :authorize
  
-  def new
-   
-  end
   
-  def create
-    binding.pry
-    @email = Pony.mail(:to => params[:to], :from => 'rpjktest.email@gmail.com', :subject => "You have been assigned a task!" :body => '<%= link_to ("Click here to see your task."), tasks_path %>')
-     if @email
-       redirect_to user_path
-     else
-       render "new"
-     end
-      
-  end
 end
