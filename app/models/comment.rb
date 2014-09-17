ee8491c77033d4f4e21d50031cc4188665dc9483
“Jen <mpjeno@gmail.com>
2014-09-17 14:57:24 -0500

    Styling for comments section

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,9 @@ class Comment < ActiveRecord::Base
   
   belongs_to :user
   belongs_to :task
+  
+  validates :user_id, presence: true
+  validates :task_id, presence: true
+  validates :comment_text, presence: true
+  
 end
