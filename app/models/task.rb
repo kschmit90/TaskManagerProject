bc436b7402251ebbdebd6251656cc3c208575f15
“Jen <mpjeno@gmail.com>
2014-09-16 22:25:48 -0500

    Added validation to ensure the same category and user are only added to a task once

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ActiveRecord::Base
   attr_accessible :complete, :deadline, :name, :project_id, :categories
 
   has_and_belongs_to_many :categories
-  has_and_belongs_to_many :users
+  has_and_belongs_to_many :users, :uniq => true
   
   scope :is_complete, where(:complete => true)
   scope :is_not_complete, where(:complete => false)
