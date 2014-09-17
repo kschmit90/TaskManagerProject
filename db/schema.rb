f93894cfacad29a7e9ca5a72bed3580ae308baaf
“Jen <mpjeno@gmail.com>
2014-09-16 21:47:41 -0500

    Debugging database issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140917021059) do
+ActiveRecord::Schema.define(:version => 20140917013434) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -44,8 +44,8 @@ ActiveRecord::Schema.define(:version => 20140917021059) do
   end
 
   create_table "tasks_users", :force => true do |t|
+    t.integer "category_id"
     t.integer "task_id"
-    t.integer "user_id"
   end
 
   create_table "users", :force => true do |t|
