ed7af54364f88c47a16df86ea24471f1da89f750
“Jen <mpjeno@gmail.com>
2014-09-17 11:06:54 -0500

    Removed "through" component of relationships.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :name
   
   has_and_belongs_to_many :tasks
-  has_many :comments, through: :tasks
+  has_many :comments
   
   validates :email, uniqueness: true
   validates :email, :password, :password_confirmation, presence: true
