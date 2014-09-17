eec9c88950d8f77d11b41a01ac85cb5ad2267656
“Kyle <kschmit90@gmail.com>
2014-09-17 10:20:28 -0500

    removed pry from tasks_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -52,7 +52,6 @@ class TasksController < ApplicationController
     else
       @task.users << @user
       
-      binding.pry
       Pony.mail(:to => @user.email, :from => 'rpjktest.email@gmail.com', :subject => 'hi ' + @user.name, :body => 'Hello there ' + @user.name + ' your task is ' + @task.name, :via => :smtp, :via_options => {:address => 'smtp.gmail.com',
       :port => '587', :authentication => :plain, :user_name => 'rpjktest.email@gmail.com', :password => 'Testpassword'})
       
