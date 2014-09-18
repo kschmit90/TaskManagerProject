b7a26b7c48afc2c049530c5e2d49a4153ed63ae4
“Kyle <kschmit90@gmail.com>
2014-09-18 11:45:21 -0500

    Activities log first draft

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,7 +12,6 @@ class ProjectsController < ApplicationController
   def create
     @project = Project.new(params[:project])
     
-    
     if @project.save
       @project.create_slug(@project)
       track_activity @project
@@ -31,6 +30,7 @@ class ProjectsController < ApplicationController
     @project = Project.find_by_slug(params[:id])
     
     if @project.update_attributes(params[:project])
+      @project.create_slug(@project)
       track_activity @project
       
       redirect_to project_path(@project), :notice => "Project updated! Way to go."
@@ -44,8 +44,15 @@ class ProjectsController < ApplicationController
   end
   
   def destroy
-    Project.find_by_slug(params[:id]).delete
+    binding.pry
     
+    @project_id = Project.find_by_slug(params[:id]).id
+    @project = Project.find_by_slug(params[:id])
+    Activity.where(trackable_type: "Project", trackable_id: @project_id).each do |a|
+      a.delete
+    end
+    Project.find_by_slug(params[:id]).delete
+        
     redirect_to projects_path, :notice => "Your project has been deleted."
   end
   
