6a96249b6889722e56a15fcec7b7d35e1574f0a8
Ryan Leichliter <leichliter.ryan@gmail.com>
2014-09-18 13:21:49 -0500

    creating dashboard.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,7 @@ class PagesController < ApplicationController
   skip_before_filter :authorize
   
   def dashboard
-
     @task = Task.all
-
-    
+    @activities = Activity.order("created_at desc")
   end
 end
