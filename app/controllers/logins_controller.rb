cbea548c7ffeafe7c26e388d26a41aec22e5f893
Ryan Leichliter <leichliter.ryan@gmail.com>
2014-09-18 11:17:06 -0500

    added style for errors, set up error messages for create users and create task.

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -5,13 +5,13 @@ class LoginsController < ApplicationController
   end
   
   def create
-    user = User.find_by_email(params[:email])
+    @user = User.find_by_email(params[:email])
     
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
       redirect_to dashboard_path
     else
-      raise "Invalid login."
+      render "new"
     end
   end
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to user_path(@user.id)
     else
-      raise "User could not be saved."
+      render "new"
     end
   end
     
