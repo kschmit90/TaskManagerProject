fef29bfc2ecc0d11097050c18fd18b942de6026e
Ryan Leichliter <leichliter.ryan@gmail.com>
2014-09-18 20:28:00 -0500

    Added a check box to dashboard. not functional.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,4 +8,18 @@ class PagesController < ApplicationController
     @tasks = Task.all
     @activities = Activity.order("created_at desc")
   end
+  
+  def update
+    @task = Task.all
+    
+    if @task.update_attributes(params[:task])
+      track_activity @task
+      
+      redirect_to dashboard_path(@task.id), :notice => "You rock!"
+    else
+      flash[:alert] = "Oops! Something went wrong. Please, try again."
+      render "dashboard"
+    end
+  end
+  
 end
