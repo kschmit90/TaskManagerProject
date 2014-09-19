87e08141bcad0f721c399039e92591544d790031
“Jen <mpjeno@gmail.com>
2014-09-18 21:31:59 -0500

    All tests are passing. They're passing multiple times ... but at least they're passing

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -11,4 +11,19 @@ RSpec.describe Project, :type => :model do
     a = Project.create({name: "Test", deadline: "2014-09-09", complete: false})
     expect(a.completed).to eq("not complete")
   end
+  
+  it "should not create a new instance if @deadline is not a valid date" do
+    a = Project.create({name: "Test", deadline: "apple"})
+    expect(a.id).to eq(nil)
+  end
+  
+  it "should set slug param equal to a downcase, no spaces version of the name" do
+    a = Project.create({name: "Test", deadline: "2014-09-09"})
+    expect(a.to_param).to eq("test")
+  end
+  
+  it "should set the slug equal to a downcase, no spaces version of the name" do
+    a = Project.create({name: "Test", deadline: "2014-09-09"})
+    expect(a.slug).to eq("test")
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'pg'
 
 require_relative "./models/project_spec.rb"
+require_relative "./models/task_spec.rb"
+require_relative "./models/user_spec.rb"
 
 RSpec.configure do |config|
 
